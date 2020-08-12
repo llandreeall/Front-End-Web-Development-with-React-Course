9c1a158eb467af0c9c350852a0aeab9e38339b58
Andreea Negoita <andreea.kage@yahoo.com>
2020-08-12 11:13:41 +0300

    Fetch post comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values){
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -109,7 +109,7 @@ class CommentForm extends Component {
         }
     }
 
-    function RenderComments({comments, addComment, dishId}) {
+    function RenderComments({comments, postComment, dishId}) {
         if (comments!= null) {
             
                 return(
@@ -126,7 +126,7 @@ class CommentForm extends Component {
                                 );
                             })}
                         </ul>
-                        <CommentForm dishId={dishId} addComment={addComment}/>
+                        <CommentForm dishId={dishId} postComment={postComment}/>
                     </div>
                 );
             
@@ -175,7 +175,7 @@ class CommentForm extends Component {
                                     
                                     <RenderDish dish = {props.dish} />
                                     <RenderComments comments = {props.comments} 
-                                        addComment={props.addComment}
+                                        postComment={props.postComment}
                                         dishId={props.dish.id}/>
                                     
                                 </div>
